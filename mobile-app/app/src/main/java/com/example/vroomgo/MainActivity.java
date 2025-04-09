601dc42aba09b9b1fea3ac8f9111224cc6875942
masum184e <masum184e@gmail.com>
2025-04-09 12:22:20 +0600

    mobile app initial setup

---
+++ mobile-app/app/src/main/java/com/example/vroomgo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.vroomgo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.vroomgo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.title.setText("Welcome to VroomGo!");
+    }
+}
