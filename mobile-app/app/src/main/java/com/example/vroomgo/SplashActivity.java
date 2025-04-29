50ce7b9ea1f6b17fb27ad39a9ab908450f362fd4
masum184e <masum184e@gmail.com>
2025-04-29 21:47:28 +0600

    splash screen uploaded

---
+++ mobile-app/app/src/main/java/com/example/vroomgo/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.vroomgo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.vroomgo.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+       new Handler().postDelayed(() -> {
+           startActivity(new Intent(SplashActivity.this, MainActivity.class));
+           finish();
+       }, 2000);
+
+    }
+
+
+}
