3bebb098c135d91b939aef15d36ccbb2011c2cb6
masum184e <masum184e@gmail.com>
2025-05-24 19:47:59 +0600

    notification btn event listner added

--- mobile-app/app/src/main/java/com/example/vroomgo/fragments/HomeFragment.java
+++ mobile-app/app/src/main/java/com/example/vroomgo/fragments/HomeFragment.java
@@ -1,18 +1,28 @@
 package com.example.vroomgo.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.vroomgo.CarDetailsActivity;
 import com.example.vroomgo.R;
 
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageView notificationBtn = view.findViewById(R.id.notification_btn);
+        notificationBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), CarDetailsActivity.class);
+            startActivity(intent);
+        });
+
         return view;
     }
 }
