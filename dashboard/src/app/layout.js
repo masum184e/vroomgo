b991fce54e22baa0bc21fb7279f16b2f7b5a8486
masum184e <masum184e@gmail.com>
2025-06-23 20:42:40 +0600

    favicon added

--- dashboard/src/app/layout.js
+++ dashboard/src/app/layout.js
@@ -1,10 +1,18 @@
 import "./globals.css"
 
+export const metadata = {
+  title: "VroomGo - Rent a Car, Embar Your Jouney",
+  description: 'Find and book premium cars quickly and easily.',
+  icons: {
+    icon: '/images/logo.png',
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-          {children}
+        {children}
       </body>
     </html>
   )
